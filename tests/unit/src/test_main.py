dbd77498c0abdd5f0bd7abf48d0e70991469393b
Anthony Herrera <anthonyherrera24@gmail.com>
2022-05-25 13:16:22 -0500

    added testing

---
+++ tests/unit/src/test_main.py
@@ -0,0 +1,50 @@
+
+import os
+os.environ["ENV"] = "test"
+from src.tnote import (
+        process_tags
+)
+import unittest
+from unittest.mock import patch
+
+class TestProcessTags(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_simple(self):
+        input_tags = "todo,later,new"
+        expected_tags = "todo,later,new"
+        output = process_tags(input_tags)
+
+        output = output.split(",")
+        expected_tags = expected_tags.split(",")
+        self.assertCountEqual(expected_tags, output)
+
+    def test_whitespace(self):
+        input_tags = "todo ,   later,new"
+        expected_tags = "todo,later,new"
+        output = process_tags(input_tags)
+
+        output = output.split(",")
+        expected_tags = expected_tags.split(",")
+        self.assertCountEqual(expected_tags, output)
+
+    def test_empty(self):
+        input_tags = "todo,later,new,,"
+        expected_tags = "todo,later,new"
+        output = process_tags(input_tags)
+
+        output = output.split(",")
+        expected_tags = expected_tags.split(",")
+        self.assertCountEqual(expected_tags, output)
+
+class TestGetKeys(unittest.TestCase):
+    def setUp(self):
+        patcher1 = patch("src.tnote.getkey")
+        self.mock_get_key = patcher1.start()
+        self.addCleanup(patcher1.stop)
+
+
+    def test_simple(self):
+        #TODO
+        pass
